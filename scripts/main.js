433e3524495c2b8cd24c37e5b739672734962f86
Mike Goodwin <goodo73@gmail.com>
2017-03-14 22:47:51 +1100

    Add random number function

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 $(function() {
 
+    $('html').removeClass('no-js').addClass('js');
+
 });
+
+// Obtain a random number between min & max (inclusive)
+function getRandomNumber (min,max) {
+    return Math.floor(Math.random() * ((max + 1) - min)) + min;
+}
